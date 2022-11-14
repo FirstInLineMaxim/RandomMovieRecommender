1722586e4f3fafe4b617a9dcd75925d6e383745d
FirstInLineMaxim <109926706+FirstInLineMaxim@users.noreply.github.com>
2022-11-14 15:47:46 +0100

    styling

--- src/Components/LikedPage.jsx
+++ src/Components/LikedPage.jsx
@@ -14,14 +14,22 @@ useEffect(()=>{
     <>
     {movies && <div className='cardContainer'>
  {movies.map((ele,i) => (
+  <>
+  <div>
       <div className='MovieCard'>
       <img src={baseUrl+"w92"+ele.poster_path} alt=""/>
       <div className='description'>
         <h4>{ele.title}</h4>
-        <p>{ele.tagline ? ele.tagline : ele.overview.slice(0,255)}</p>
+        <p>{ele.tagline ? ele.tagline : ele.overview.slice(0,100)}</p>
         <a href=""></a>
         </div>
+        <div className='ratings'>
+        {Object.values(ele.genres).map(ele => <h6>{ele.name}</h6>)}
+        </div>
       </div>
+      <div className='lang'><p>Avaible in:</p>{ele.spoken_languages.map(ele=> <p>{ele.english_name}</p>)}</div>
+      </div>
+      </>
     ))}
 </div>}
 
