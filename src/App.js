9536f1c0f81f5020905492b6de13c6fd697e87f6
FirstInLineMaxim <109926706+FirstInLineMaxim@users.noreply.github.com>
2022-11-14 11:56:39 +0100

    path

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import GetMovieDB from "./GetMovieDB";
+import FilmPage from "./Components/FilmPage";
 import {BrowserRouter as Router,Routes,Route,Link} from "react-router-dom";
 
 
@@ -17,7 +18,7 @@ function App() {
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
         <Routes>
-          <Route path="/about" element={""}/>
+          <Route path="/film" element={<FilmPage/>}/>
           <Route path="/saved"element={""} />
           <Route path="/" element={<GetMovieDB/>} />
         </Routes>
