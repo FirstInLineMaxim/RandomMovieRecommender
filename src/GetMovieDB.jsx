d7d3af57c1f881f46b9116aa8d686ac4f94d7d0d
FirstInLineMaxim <109926706+FirstInLineMaxim@users.noreply.github.com>
2022-11-14 16:08:29 +0100

    another change

--- src/GetMovieDB.jsx
+++ src/GetMovieDB.jsx
@@ -25,7 +25,7 @@ export default function GetMovieDB() {
 
     function getNumber() {
         //Nummer zwischen 10000 und 1
-        setNumber(Math.floor(Math.random() * 1000 + 1));
+        setNumber(Math.floor(Math.random() * 1048288 + 1));
     }
 
     function verifystatusCode(data) {
