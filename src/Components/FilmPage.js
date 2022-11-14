238add29f9cb1102eec21726da53b0307511159d
FirstInLineMaxim <109926706+FirstInLineMaxim@users.noreply.github.com>
2022-11-14 15:58:44 +0100

    leuft

--- src/Components/FilmPage.js
+++ src/Components/FilmPage.js
@@ -66,10 +66,9 @@ export default function FilmPage({ data }) {
     }
     useEffect(() => {
         const parsed = JSON.parse(localStorage.getItem("savedMovies"));
-        // if (parsed) {
-        //     setSave(parsed);
-        // }
-        console.log("parsed:", parsed);
+        if (parsed) {
+            setSave(parsed);
+        }
     }, []);
 
     return (
