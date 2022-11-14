5cf10054fd36059bad36b9266c0a0702f555484c
Marwa Laatil <elatilmarwa@gmail.com>
2022-11-14 11:57:27 +0100

    film data img url

--- src/Components/FilmPage.js
+++ src/Components/FilmPage.js
@@ -4,12 +4,17 @@ import config from "../config.json";
 export default function filmPage({ data }) {
     const { images } = config;
     const { base_url } = images;
+    const { logo_sizes } = images;
+    console.log("0", logo_sizes[5]);
     console.log(base_url);
     let title, img, homepageUrl, overview, release_date;
     if (data) {
         console.log("result", data);
         title = data.original_title;
-        img = `https://image.tmdb.org/t/p/w500/${data.backdrop_path}`;
+        console.log(base_url + data.backdrop_path);
+        img = `${base_url}${logo_sizes[5]}${data.backdrop_path}`;
+        // img = `https://image.tmdb.org/t/p/w500/${data.backdrop_path}`;
+
         overview = data.overview;
         homepageUrl = data.homepage;
         release_date = data.release_date;
